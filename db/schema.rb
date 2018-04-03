6bfcbba9f62905026d264a278bb9d4d2e55219ea
nguyen ngoc dong <nguyenngocdongfa@gmail.com>
2018-04-03 09:09:10 +0000

    login,logout,sigup,signin

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180328022248) do
+ActiveRecord::Schema.define(version: 20180403085156) do
 
   create_table "books", force: :cascade do |t|
     t.string   "title"
@@ -41,9 +41,12 @@ ActiveRecord::Schema.define(version: 20180328022248) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "name"
+    t.integer  "age"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["name"], name: "index_users_on_name", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
